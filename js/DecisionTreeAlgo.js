66f0f8cc7c2972c56ec5de8ad5ef9af716997882
LastStep <drohanyadav@gmail.com>
2020-07-24 10:11:24 +0530

    read csv

---
+++ js/DecisionTreeAlgo.js
@@ -0,0 +1,28 @@
+let data = [];
+
+// Grab CSV Data
+// d3.csv("data/iris.csv").then(function(csvData){
+//     csvData.forEach(function(d){
+//         data.push(d);
+//     });
+// });
+
+class DataColumns {
+
+	constructor(node, value) {
+		this.node = node;
+		this.value.push(value);
+	}
+}
+
+function readcsv(publicSpreadsheetUrl) {
+	Tabletop.init( { key: publicSpreadsheetUrl,
+											callback: showInfo,
+											simpleSheet: true } )
+}
+
+function showInfo(data, tabletop) {
+	console.log(data);
+}
+
+readcsv('https://docs.google.com/spreadsheets/d/1pct_LvoU40u6hu4GvuNXeMkJtCAWT-lgJNW_pgiOPg8');
